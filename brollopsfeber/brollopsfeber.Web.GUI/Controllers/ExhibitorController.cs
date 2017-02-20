699d80ff3fdd101cff36bd442adefbd8f85e0330
Jesper Oscarsson <jejo7777@gmail.com>
2017-02-20 19:40:30 +0100

    Added ninject & Moq

--- brollopsfeber/brollopsfeber.Web.GUI/Controllers/ExhibitorController.cs
+++ brollopsfeber/brollopsfeber.Web.GUI/Controllers/ExhibitorController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using brollopsfeber.Domain.Abstract;
 
 namespace brollopsfeber.Web.GUI.Controllers
 {
     public class ExhibitorController : Controller
     {
+        private IExhibitorRepository _repo;
+
+        public ExhibitorController(IExhibitorRepository repo)
+        {
+            _repo = repo;
+        }
+
         public ViewResult ViewExhibitors()
         {
-            return View();
+            return View(_repo.Exhibitors);
         }
     }
 }
---
+++ brollopsfeber/brollopsfeber.Web.GUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,42 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Moq;
+using brollopsfeber.Domain.Entites;
+using brollopsfeber.Domain.Abstract;
+using brollopsfeber.Domain.Concrete;
+
+namespace brollopsfeber.Web.GUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+
+            Mock<IExhibitorRepository> mock = new Mock<IExhibitorRepository>();
+            mock.Setup(m => m.Exhibitors).Returns(new List<Exhibitor>
+            {
+                new Exhibitor() { Name = "Jonas Strandberg" },
+                new Exhibitor() { Name = "Erik Falkberg" }
+            });
+
+            _kernel.Bind<IExhibitorRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+    }
+}
