82e5d3f4d540a7115c183b49579f6f6e981b1c6b
Jesper Oscarsson <jejo7777@gmail.com>
2017-02-20 18:59:39 +0100

    Added new class for exhibitors and created default & exhibitor controllers.

---
+++ brollopsfeber/brollopsfeber.Web.GUI/Controllers/ExhibitorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace brollopsfeber.Web.GUI.Controllers
+{
+    public class ExhibitorController : Controller
+    {
+        public ViewResult ViewExhibitors()
+        {
+            return View();
+        }
+    }
+}
